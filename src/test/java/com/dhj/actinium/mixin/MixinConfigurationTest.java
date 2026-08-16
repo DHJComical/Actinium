6d5fd74c0835b78fc705e929769b323bedb3853a
DHJComical <dhjcomical@163.com>
2026-08-16 20:45:01 +0800

    fix(ci): repair merge-broken mixin config test lists
    The web merge of the VoxelMap compatibility change (#49) into main lost
    the trailing commas in the expected mixin config lists of MixinLateTest
    and MixinConfigurationTest, breaking compileTestJava on CI.
    
    Restore the commas between mixins.actinium.voxelmap.json and
    mixins.actinium.extrautils2.json in both lists.

--- src/test/java/com/dhj/actinium/mixin/MixinConfigurationTest.java
+++ src/test/java/com/dhj/actinium/mixin/MixinConfigurationTest.java
@@ -50,7 +50,7 @@ class MixinConfigurationTest {
         "mixins.actinium.revoui.json",
         "mixins.actinium.betterfoliage.json",
         "mixins.actinium.ccl.json",
-        "mixins.actinium.voxelmap.json"
+        "mixins.actinium.voxelmap.json",
         "mixins.actinium.extrautils2.json"
     );
     private static final List<String> CONFIGS = Stream.concat(
